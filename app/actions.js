3ad92a65be2ac77425ffcd83b780d2fa6e540a15
Guillermo Rauch <rauchg@gmail.com>
2024-02-11 11:02:01 -0800

    rename actions

--- app/actions.js
+++ app/actions.js
@@ -2,7 +2,7 @@
 import { db, todosTable } from "./db";
 import { redirect } from "next/navigation";
 
-export async function submit(form: FormData) {
+export async function submit(form) {
   const text = form.get("text") + "";
   if (!/^[\p{Emoji}]+$/u.test(text)) return;
   await db.insert(todosTable).values({ text });
